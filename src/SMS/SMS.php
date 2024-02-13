8c1fe51ecb678105c23839db5d9064722949f099
dmburu <douglasmburu105@gmail.com>
2024-02-13 20:27:54 +0300

    add SMS facade  accessor

--- src/SMS/SMS.php
+++ src/SMS/SMS.php
@@ -8,6 +8,11 @@ use Illuminate\Support\Facades\Facade;
 
 class SMS extends Facade
 {
+    /**
+     * Class SMS
+     * @package Waihiga\Phpsmsmail\SMS
+     * @method send($number, $message)
+     */
     public static function getFacadeAccessor()
     {
         return 'sms';
